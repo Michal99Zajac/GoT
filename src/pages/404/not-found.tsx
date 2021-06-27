6f7f97406688fb3aa3778f78b67cceb6d85692bb
Michal99Zajac <michal.99.zajac@gmail.com>
2021-06-27 12:16:59 +0200

    add 404 page to the project

--- src/pages/404/not-found.tsx
+++ src/pages/404/not-found.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import ThemeContext from '../../context/theme-context';
+import './not-found.css';
 
 
+/**
+ * 404 page
+ */
 export default function NotFound() {
+  const theme = useContext(ThemeContext);
+  const snows = Array.from({length: 20}, (v, k) => k + 1); // snowflakes array
+
+  /**
+   * add snowflake animations
+   */
+  useEffect(() => {
+    const snowflakes = document.querySelectorAll('.snowflake');
+
+    snowflakes.forEach(s => {
+      const delayOne = Math.floor(Math.random() * 20);
+      const delayTwo = Math.floor(Math.random() * 20);
+      const left = Math.floor(Math.random() * 100);
+
+      s.setAttribute('style', `left: ${left}%;
+      -webkit-animation-delay: ${delayOne}s;
+      animation-delay: ${delayOne}s;
+      `)
+    })
+  }, [])
+
   return (
-    <div>
-      404
+    <div className='fill-all not-found'>
+      <h1 className={`h1-not-found-${theme.theme}`}>400</h1>
+      { snows.map((s, idx) => <span key={idx} className={`snowflake snow-${theme.theme}}`}></span>)  }
     </div>
   );
 }
