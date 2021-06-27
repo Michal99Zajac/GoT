187d9f77a0e8426aeadb799d9c0f616a76413c8b
Michal99Zajac <michal.99.zajac@gmail.com>
2021-06-27 08:46:12 +0200

    add skeleton of house page

--- src/components/house/house.tsx
+++ src/components/house/house.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Table from '../table/table';
+import Row from '../table/row/row';
 import styles from './house.module.css';
 
 
@@ -7,10 +9,24 @@ interface HouseProps {
   id?: string;
 }
 
+/**
+ * House Component for display and fetch data about house
+ * 
+ * className? (string) - additional class for componwnt
+ * id? (string) - id of component
+ */
 export default function House(props: HouseProps): JSX.Element {
   return (
-    <div>
-      house
-    </div>
+    <Table
+      // loading={true}
+      className={styles.table}
+      direction='horizontal'
+      columns={
+        ['region', 'words', 'titles', 'seats', 'has died out',
+          'has overlord', 'number of cadet branches']
+      }
+    >
+      <Row data={['region', 'words', 'titles', 'seats', 'has died out', 'has overlord', 'number of cadet branches']} />
+    </Table>
   )
 }
--- src/pages/houses/houses.tsx
+++ src/pages/houses/houses.tsx
@@ -1,16 +1,23 @@
 import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import ThemeContext from '../../context/theme-context';
 import House from '../../components/house/house';
+import { ReactComponent as ArrowSvg } from '../../assets/svg/arrow.svg'
 import './houses.css';
 
 
 export default function Houses() {
   const theme = useContext(ThemeContext);
+  const history = useHistory()
 
   return (
     <div className='fill-all houses-page'>
-      <p className={`grid-arms arms-h1 arms-${theme.theme}`}>Text describing the coat of arms of this House</p>
+      <p className={`grid-arms arms-h1 text-${theme.theme}`}>Text describing the coat of arms of this House</p>
       <div className='grid-house house'>
+        <div className='header'>
+          <ArrowSvg className={'back-arrow'} onClick={() => history.goBack()} />
+          <p className={`house-name text-${theme.theme}`}>name of house</p>
+        </div>
         <House />
       </div>
     </div>
